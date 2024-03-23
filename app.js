db44b81d01d387547f025de5201e1c72fe9dab3a
prammmoe <ikhwanpramuditha05@gmail.com>
2024-03-24 00:10:48 +0700

    change cors port

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-import express, { urlencoded, json } from 'express';
-import logger from 'morgan';
-import cors from 'cors';
+import express, { urlencoded, json } from "express";
+import logger from "morgan";
+import cors from "cors";
 
 // Instantiate base app and port
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Import routing (controller will also implicitly imported in this routing)
-import { productRoute } from './src/routes/productRoute.js';
-import { userRoute } from './src/routes/userRoute.js';
-import { categoryRoute } from './src/routes/categoryRoute.js';
+import { productRoute } from "./src/routes/productRoute.js";
+import { userRoute } from "./src/routes/userRoute.js";
+import { categoryRoute } from "./src/routes/categoryRoute.js";
 
 // Define using middleware function
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
 // Connect to front-end using CORS
-const corsBaseUrl = 'http://localhost:3000';
+const corsBaseUrl = process.env.PORT || 3000;
 
 // CORS middleware
 app.use(
